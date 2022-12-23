6ddabb1ce5a646fc1621db1490a280839502d3d3
kianzzarrin <kian.zarrin@gmail.com>
2022-12-23 20:23:51 +0200

    initial commit

--- ExperimentMod/UserMod.cs
+++ ExperimentMod/UserMod.cs
@@ -6,25 +6,8 @@
     using KianCommons;
     using System.Diagnostics;
 
-    public class UserMod2 : IUserMod
-    {
-        public string Name => "break";
-        public string Description => Name;
-
-        public void OnSettingsUI(UIHelperBase helper)
-        {
-
-        }
-    }
-
     public class UserMod : IUserMod {
-        static UserMod() {
-            Log.Debug("ExperimentMod.UserMod static constructor called!" + Environment.StackTrace);
-        }
-
-        public static Version ModVersion => typeof(UserMod).Assembly.GetName().Version;
-        public static string VersionString => ModVersion.ToString(2);
-        public string Name => "Experiment Mod " + VersionString;
+        public string Name => "Throw exceptions";
         public string Description => "control Road/junction transitions";
         const string HARMONY_ID = "Kian.ExperimentMod";
 
@@ -39,10 +22,7 @@
             HarmonyHelper.DoOnHarmonyReady(() => HarmonyUtil.InstallHarmony(HARMONY_ID));
 
             if (HelpersExtensions.InGame) {
-                for (ushort nodeID =1; nodeID < NetManager.MAX_NODE_COUNT; ++nodeID) {
-                    if(nodeID.ToNode().m_flags.CheckFlags(NetNode.Flags.Created | NetNode.Flags.Transition, NetNode.Flags.Deleted))
-                        NetManager.instance.UpdateNode(nodeID);
-                }
+
             }
         }
 
